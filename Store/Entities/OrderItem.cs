5ed095552d9f1192f8e665ba353b0aa333e3bbfa
Allyson Reis <allysonreis1@hotmail.com>
2019-02-15 15:29:25 -0300

    Creating a new class OrderItem

---
+++ Store/Entities/OrderItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Store.Entities
+{
+    class OrderItem
+    {
+        //Properties
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public double subTotal ()
+        {
+            return Price * Quantity;
+        }
+    }
+}
