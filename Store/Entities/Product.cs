bf685cb293a4d0b2905e90c7a0d8dd9da4655c65
Allyson Reis <allysonreis1@hotmail.com>
2019-02-15 15:28:41 -0300

    Add a new comment

--- Store/Entities/Product.cs
+++ Store/Entities/Product.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Store.Entities
+﻿namespace Store.Entities
 {
     class Product
     {
+        //Properties
         public string Name { get; set; }
         public double Price { get; set; }
     }
