0bd41677b46cde7dfc03f48ba36a440c24504751
Allyson Reis <allysonreis1@hotmail.com>
2019-02-15 16:01:32 -0300

    Creating one class and one Enum

---
+++ Store/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace Store.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PENDING_PAYMENT = 0,
+        PROCESSING = 1,
+        SHIPPED = 2,
+        DELIVERED = 3
+    }
+}
---
+++ Store/Entities/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Store.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Store.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus MyProperty { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+    }
+}
