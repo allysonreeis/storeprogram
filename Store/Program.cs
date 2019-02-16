afee71a0cd159aea6e42a1f3064783005465bfca
Allyson Reis <allysonreis1@hotmail.com>
2019-02-16 18:01:05 -0300

    Writing code for obtain the user data

--- Store/Program.cs
+++ Store/Program.cs
@@ -6,7 +6,25 @@ namespace Store
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+
+            Console.WriteLine("Enter order data:");
+            Console.Write("Status: ");
+            Console.Write("How many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");//Interpolation
+                Console.WriteLine("Product name: ");
+                Console.WriteLine("Product price: ");
+                Console.WriteLine("Quantity: ");
+            }
         }
     }
 }
